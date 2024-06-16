010a7b51ac9060863b0d02acc81695739d3e08e6
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2024-06-16 14:02:44 +0300

    new profile pic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./NavLinks";
 import Link from "next/link";
 
 import { useSession } from "next-auth/react";
-import { ShoppingCartIcon } from "@heroicons/react/20/solid";
+import { ShoppingCartIcon,UserCircleIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
 import CartProductNumber from "./CartProductNumber";
 
@@ -17,7 +17,6 @@ const Navbar: React.FC = () => {
       <div className="flex-1">
         <Link href="/">
         <div className="btn btn-ghost text-xl flex items-center">
-
           <img
             className="h-10 w-10"
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABpklEQVR4nO2Zy0oDQRBFG/0CF4FkUH9ExBcu1HyOSXAbstG/8Y1KXJh/iWShJMGFUcyR1hZDIBmZ6pfQF4ZhFrlVh7rDNBWlhAKqQBt4obj0b++BfWk/RSFa2FfTN4SehNYrcAhUBF4VoGa88DoZEwWtmkXPuvG8s+X5l6JDUzQzzx1BnDrGIzPPfZ8gHxZBHqZACAHSsOjZCAkyMtmWvOyZgRiFBHEiFQBkE7gEeoK+e8ZjPSTIgmXfL9n0zCuYQOYJI+VL/3YiwMGMU64TkKlTcdWWeZPZcgkyqZbUeI/58gWCaDL6JEo8IG2JcT8ikKHE+D3HfNUixEpOLVyCXOkGLEAsm2OKM5An4lFfAnJNPLqRgOwA49AEfPewWxjEwNQDw4yBIynEorlvAKdAF3j2dHWBM2BrspeiIBdASQUWUNLvq8TgDRgAJ8AaUAaWPF1lU/P458MsBYlGKoGQJuJEKkWLFC0nUilapGg5kUrRIkXLiVSKFilaTqRStIhqrzWQTERvEmPRrQRkO+21Ytprqd/J6P/T9Y7p0eNeS9c616nIa/ATcA77JAZyghEAAAAASUVORK5CYII="
@@ -49,10 +48,7 @@ const Navbar: React.FC = () => {
             className="btn btn-ghost btn-circle avatar"
           >
             <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+            <UserCircleIcon className="h-10 w-10" />
             </div>
           </div>
           <ul
